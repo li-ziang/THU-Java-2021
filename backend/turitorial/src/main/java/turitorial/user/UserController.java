732398965756fcaafbd8fac89ff3b3f84d6562c1
黄立言 <hly19@huangliyandeMacBook-Pro.local>
2021-08-18 18:04:32 +0800

    修改注册登录的返回值为json

--- backend/turitorial/src/main/java/turitorial/user/UserController.java
+++ backend/turitorial/src/main/java/turitorial/user/UserController.java
@@ -1,57 +1,78 @@
 package turitorial.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import turitorial.Status;
-import turitorial.user.UserRepository;
 
 import javax.validation.Valid;
 import java.util.List;
+import org.json .*;
+
 @RestController
 public class UserController {
     @Autowired
     UserRepository userRepository;
     @PostMapping("/users/register")
-    public Status registerUser(@Valid @RequestBody User newUser) {
+    public String registerUser(@Valid @RequestBody User newUser) {
+        JSONObject jsonObject = new JSONObject();
         List<User> users = userRepository.findAll();
         System.out.println("New user: " + newUser.toString());
         for (User user : users) {
             System.out.println("Registered user: " + newUser.toString());
             if (user.equals(newUser)) {
                 System.out.println("User Already exists!");
-                return Status.USER_ALREADY_EXISTS;
+                jsonObject.put("code", "400");
+                jsonObject.put("content", "user already exists");
+                return jsonObject.toString();
             }
         }
         userRepository.save(newUser);
-        return Status.SUCCESS;
+        jsonObject.put("code", "200");
+        jsonObject.put("content", "registered successfully");
+        return jsonObject.toString();
     }
     @PostMapping("/users/login")
-    public Status loginUser(@Valid @RequestBody User user) {
+    public String loginUser(@Valid @RequestBody User user) {
+        JSONObject jsonObject = new JSONObject();
         List<User> users = userRepository.findAll();
         for (User other : users) {
             if (other.equals(user)) {
                 user.setLoggedIn(true);
                 userRepository.save(user);
-                return Status.SUCCESS;
+                jsonObject.put("code", "200");
+                jsonObject.put("content", "login successfully");
+                return jsonObject.toString();
             }
         }
-        return Status.FAILURE;
+        jsonObject.put("code","400");
+        jsonObject.put("content", "login failed");
+        return jsonObject.toString();
     }
     @PostMapping("/users/logout")
-    public Status logUserOut(@Valid @RequestBody User user) {
+    public String logUserOut(@Valid @RequestBody User user) {
+        JSONObject jsonObject = new JSONObject();
         List<User> users = userRepository.findAll();
         for (User other : users) {
             if (other.equals(user)) {
                 user.setLoggedIn(false);
                 userRepository.save(user);
-                return Status.SUCCESS;
+                jsonObject.put("code", "200");
+                jsonObject.put("content", "logout successfully");
+                return jsonObject.toString();
             }
         }
-        return Status.FAILURE;
+        jsonObject.put("code","400");
+        jsonObject.put("content", "did not login");
+        return  jsonObject.toString();
     }
     @DeleteMapping("/users/all")
-    public Status deleteUsers() {
+    public String deleteUsers() {
+        JSONObject jsonObject = new JSONObject();
         userRepository.deleteAll();
-        return Status.SUCCESS;
+        jsonObject.put("code","200");
+        return jsonObject.toString();
     }
 }
