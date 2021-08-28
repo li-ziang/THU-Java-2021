987bd35709467bad9402971f0a25517b949ebb7b
liziang <983417395@qq.com>
2021-08-28 19:32:15 +0800

    object activity

--- app/src/main/java/com/java/liziang/ObjectActivity.java
+++ app/src/main/java/com/java/liziang/ObjectActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Object extends AppCompatActivity {
+public class ObjectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
