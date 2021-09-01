56c435639fd22151f6420918b946d4306d25d01f
liziang <983417395@qq.com>
2021-09-01 17:05:32 +0800

    删除了冗余的message代码

--- app/src/main/java/com/java/liziang/MainActivity.java
+++ app/src/main/java/com/java/liziang/MainActivity.java
@@ -58,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.user_image).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-//                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                 startActivity(new Intent(MainActivity.this, ChannelListActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//                 startActivity(new Intent(MainActivity.this, ChannelListActivity.class));
             }
         });
         findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener(){
